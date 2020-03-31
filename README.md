943ca9e9cf7b92c2c4548d5fdbf1435cb63ae7d0
Trent Mackness <trent@quiqpay.io>
2020-03-31 15:42:01 +1000

    inital commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Dropbox Ignore node_modules
+
+This NodeJS script works as a CLI that tells Dropbox to ignore the node_modules directory.
+
+As per [Dropbox Documentation](https://help.dropbox.com/en-us/files-folders/restore-delete/ignored-files) you can see the simple command used to do this.
+
+# Usage
+
+Navigate to the working directory that contains your NodeJS project with the node_modules directory. Then you can run the this script.
+
+You may use the command `dropignore` or `di` for short.
+
+```bash
+cd /Users/username/node/project
+di
+```
+
+## Notes
+
+Should work with Windows, MacOS and Linux, however, only tested with MacOS.
---
+++ index.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+const { exec } = require("child_process");
+const fs = require('fs');
+
+(async () => {
+  try {
+    const cwd = process.cwd();
+    const modulesPath = cwd + '/node_modules';
+
+    // check if node_modules exists
+    const nodeModulesExist = fs.existsSync(modulesPath);
+    if (!nodeModulesExist) {
+      throw new Error(`A node_modules directory does not exist in this location - ${cwd}`)
+    }
+
+    // shell command
+    const platform = process.platform;
+    const command = (
+      platform === "win32"
+        ? `Set-Content -Path '${modulesPath}' -Stream com.dropbox.ignored -Value 1`
+        : platform === "darwin"
+          ? `xattr -w com.dropbox.ignored 1 ${modulesPath}`
+          : `attr -s com.dropbox.ignored -V 1 ${modulesPath}`
+    );
+
+    // execute shell command
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        throw error;
+      }
+      if (stderr) {
+        throw stderr;
+      }
+      
+      console.log("Dropbox is now ignoring node_modules.");
+    });
+    
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+  }
+})();
